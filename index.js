2d58e0a28f4b136d4254440f6562e5e1c2629648
Jahson Diego <jdiego847@gmail.com>
2021-10-29 22:31:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(people, surprise){
+    let message = [];
+    for (let i = 0; i < people.length; i++){
+        message[i] = `Thank you, ${people[i]}, for the wonderful ${surprise} gift!`;
+        debugger;
+    }
+return message;
+}
+function countDown(num){
+    while (num >= 0){
+        console.log(num--);
+    }
+}
